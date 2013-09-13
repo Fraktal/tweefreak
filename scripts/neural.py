c8bc93be42122f0eec125839089ba44ad9499d21
Todd Ervin <servin@unca.edu>
2013-09-13 10:51:32 -0400

    added template for neural.py

---
+++ scripts/neural.py
@@ -0,0 +1,12 @@
+import numpy as np
+import neurolab as nl
+
+# Create train samples
+input = np.random.uniform(-0.5, 0.5, (10, 2))
+target = (input[:, 0] + input[:, 1]).reshape(10, 1)
+
+# Create network with 2 inputs, 5 neurons in input layer and 1 in output layer
+net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5]], [5, 1])
+
+# Train process
+err = net.train(input, target, show=15)
